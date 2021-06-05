8652eb634867395cbf38b651f378661caaad063b
Smit Sakariya <ayirakas28@gmail.com>
2021-06-05 15:20:09 -0400

    Added a smaller button texture
    - Added a medium sized default texture for button
    - Renamed older texture name to "BigButton"

--- PauseMenuEdits/src/main/java/io/github/realcaptainindia/pausemenuedits/config/DefaultButtons.java
+++ PauseMenuEdits/src/main/java/io/github/realcaptainindia/pausemenuedits/config/DefaultButtons.java
@@ -7,25 +7,25 @@ public class DefaultButtons {
 	public static Map<String, CustomButton> getButtons() {
 		Map<String, CustomButton> list = new LinkedHashMap<String, CustomButton>();
 		list.put("UnPause Button", new CustomButton(-102, 70, 204, 20,
-				"pausemenuedits:textures/defaultbuttons/button_unselected.png", 0, 0, "opengui", "Unpause"));
+				"pausemenuedits:textures/defaultbuttons/BigButton.png", 0, 0, "opengui", "Unpause"));
 
 		list.put("Advancements Button", new CustomButton(-102, 45, 98, 20,
-				"pausemenuedits:textures/defaultbuttons/button_unselected.png", 0, 0, "opengui", "Advancement"));
+				"pausemenuedits:textures/defaultbuttons/MediumButton.png", 0, 0, "opengui", "Advancement"));
 
 		list.put("Statistics Button", new CustomButton(4, 45, 98, 20, 
-				"pausemenuedits:textures/defaultbuttons/button_unselected.png", 0, 0, "opengui", "Statistics"));
+				"pausemenuedits:textures/defaultbuttons/MediumButton.png", 0, 0, "opengui", "Statistics"));
 
 		list.put("Options Button", new CustomButton(-102, 20, 204, 20,
-				"pausemenuedits:textures/defaultbuttons/button_unselected.png", 0, 0, "opengui", "Options"));
+				"pausemenuedits:textures/defaultbuttons/BigButton.png", 0, 0, "opengui", "Options"));
 
 		list.put("Mods List Button", new CustomButton(-102, -5, 98, 20,
-				"pausemenuedits:textures/defaultbuttons/button_unselected.png", 0, 0, "opengui", "Mods List"));
+				"pausemenuedits:textures/defaultbuttons/MediumButton.png", 0, 0, "opengui", "Mods List"));
 
 		list.put("Lan Button", new CustomButton(4, -5, 98, 20,
-				"pausemenuedits:textures/defaultbuttons/button_unselected.png", 0, 0, "opengui", "Lan"));
+				"pausemenuedits:textures/defaultbuttons/MediumButton.png", 0, 0, "opengui", "Lan"));
 
 		list.put("Main Menu Button", new CustomButton(-102, -30, 204, 20,
-				"pausemenuedits:textures/defaultbuttons/button_unselected.png", 0, 0, "opengui", "Quit"));
+				"pausemenuedits:textures/defaultbuttons/BigButton.png", 0, 0, "opengui", "Quit"));
 		return list;
 	}
 }
